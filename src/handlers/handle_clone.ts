1a96dcc0bb4396a297953b8340b6c7feec666ce1
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2019-04-09 09:07:15 +0530

    issue with remove .git folder solved by using fs-extra

--- src/handlers/handle_clone.ts
+++ src/handlers/handle_clone.ts
@@ -2,6 +2,8 @@ import {
     execSync
 } from "child_process";
 import { runCommand } from "../helpers";
+import { removeSync } from "fs-extra";
+import * as Path from "path";
 
 const typescriptProjectUrl = "https://github.com/ujjwalguptaofficial/fortjs-typescript-starter";
 const javascriptProjectUrl = "https://github.com/ujjwalguptaofficial/fortjs-javascript-starter";
@@ -22,12 +24,19 @@ export const handleClone = function (type, name) {
         else {
             console.log("setting up project....");
             // Remove the .git directory
-            execSync(`rm -rf ./${name}/.git`);
+            // execSync(`rm -rf ./${name}/.git`);
+            const gitDirectory = Path.join(process.cwd(), name, '.git/')
+            //  ${}/${name}.git/`;
+            console.log('removing using fs', gitDirectory);
+            removeSync(gitDirectory)
             console.log("downloading dependency....");
             // downloading dependencies
             runCommand(`cd ${name} && npm install`).then(function (code) {
                 if (code != 0) {
                     console.log(`unable to install dependencies, process exited with code ${code.toString()}`)
+                }
+                else {
+                    console.log(`new project ${name} created`);
                 }
             })
         }
