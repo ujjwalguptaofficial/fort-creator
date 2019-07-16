acbfa3934263616a75f709f4ef47e5d48386b782
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2019-07-16 09:29:21 +0530

    added cross-env command

--- src/handlers/deploy_handler.ts
+++ src/handlers/deploy_handler.ts
@@ -1,7 +1,7 @@
 import { runCommand } from "../helpers";
 
 export const handleDeploy = function (deployFolderName: string) {
-    runCommand(`BUILD_FOLDER=${deployFolderName} npm run deploy`).then(function (code) {
+    runCommand(`cross-env BUILD_FOLDER=${deployFolderName} npm run deploy`).then(function (code) {
         if (code == 0) {
             console.log("build created for deploy")
         } else {
