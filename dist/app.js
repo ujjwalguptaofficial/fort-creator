17eab0c6ead93c51211330a9ce1bb69ba343fabc
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2019-07-18 07:58:48 +0530

    release v 1.1.5

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /*!
- * @license :fort-creator - V1.1.4 - 26/06/2019
+ * @license :fort-creator - V1.1.5 - 16/07/2019
  * https://github.com/ujjwalguptaofficial/fort-creator
  * Copyright (c) 2019 @Ujjwal Gupta; Licensed Apache-2.0
  */
@@ -203,7 +203,7 @@ module.exports = __webpack_require__(/*! /home/ujjwal/projects/opensource/fort-c
 /***/ }),
 /* 9 */
 /*!***********************************!*\
-  !*** ./src/index.ts + 18 modules ***!
+  !*** ./src/index.ts + 19 modules ***!
   \***********************************/
 /*! exports provided: processCommand */
 /*! all exports used */
@@ -322,6 +322,11 @@ var runCmdSync = function (cmd) {
     Object(external_child_process_["execSync"])(cmd, { stdio: 'inherit' });
 };
 
+// CONCATENATED MODULE: ./src/helpers/set_env.ts
+var setEnv = function (key, value) {
+    process.env[key] = value;
+};
+
 // CONCATENATED MODULE: ./src/helpers/index.ts
 
 
@@ -331,16 +336,64 @@ var runCmdSync = function (cmd) {
 
 
 
+
 // CONCATENATED MODULE: ./src/handlers/deploy_handler.ts
+var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (undefined && undefined.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 
 var handleDeploy = function (deployFolderName) {
-    runCommand("BUILD_FOLDER=" + deployFolderName + " npm run deploy").then(function (code) {
-        if (code == 0) {
-            console.log("build created for deploy");
-        }
-        else {
-            console.log("unable to create build for deploy, process exited with code " + code.toString());
-        }
+    return __awaiter(this, void 0, void 0, function () {
+        var code;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    setEnv('BUILD_FOLDER', deployFolderName);
+                    setEnv('NODE_ENV', 'production');
+                    return [4 /*yield*/, runCommand("npm run deploy")];
+                case 1:
+                    code = _a.sent();
+                    if (code == 0) {
+                        console.log("build created for deploy");
+                    }
+                    else {
+                        console.log("unable to create build for deploy, process exited with code " + code.toString());
+                    }
+                    return [2 /*return*/];
+            }
+        });
     });
 };
 
@@ -437,7 +490,7 @@ var external_cli_spinner_ = __webpack_require__(6);
 var external_os_ = __webpack_require__(7);
 
 // CONCATENATED MODULE: ./src/handlers/handle_clone.ts
-var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
+var handle_clone_awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
@@ -445,7 +498,7 @@ var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _argume
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __generator = (undefined && undefined.__generator) || function (thisArg, body) {
+var handle_clone_generator = (undefined && undefined.__generator) || function (thisArg, body) {
     var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
     return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
     function verb(n) { return function (v) { return step([n, v]); }; }
@@ -493,9 +546,9 @@ var handle_clone_SpinnerHelper = /** @class */ (function () {
     return SpinnerHelper;
 }());
 var handleClone = function (type, name) {
-    return __awaiter(this, void 0, void 0, function () {
+    return handle_clone_awaiter(this, void 0, void 0, function () {
         var cloneProjPath, command, exitCode, err_1;
-        return __generator(this, function (_a) {
+        return handle_clone_generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
                     if (Object(external_fs_extra_["pathExistsSync"])(name)) {
@@ -527,6 +580,7 @@ var handleClone = function (type, name) {
                         handle_clone_SpinnerHelper.stop();
                         handle_clone_SpinnerHelper.init("downloading dependency");
                         // downloading dependencies
+                        // runCmdSync(`npm i cross-env -g`);
                         runCmdSync("cd " + name + " && npm install && npm install fortjs@latest");
                         handle_clone_SpinnerHelper.stop();
                         if (exitCode != 0) {
