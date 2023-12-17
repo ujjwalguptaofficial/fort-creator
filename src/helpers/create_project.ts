902a74d9cd5f3407ddd5a6c0892cbe948882b42b
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2023-12-17 13:36:56 +0400

    update messages with emojis

--- src/helpers/create_project.ts
+++ src/helpers/create_project.ts
@@ -22,16 +22,18 @@ export const createProject = async function (type, folderName) {
     var templatePath = "https://github.com/ujjwalguptaofficial/fortjs-templates/tree/main/" + type;
     try {
 
-        SpinnerHelper.init("Setting up project");
+        SpinnerHelper.init("🚀 Setting up the project");
         const downloadStat = await ghDownload(templatePath, projectPath, {
-            muteLog: false,
+            muteLog: true,
         });
+
         if (!downloadStat.success) {
-            const err = `error - ${downloadStat.error}, project path = ${projectPath}, template path = ${templatePath}`;
-            throw new Error(err || 'Can not download the template repo, please contact author if you are seeing this error.');
+            const err = `❌ Error: ${downloadStat.error}\nProject Path: ${projectPath}\nTemplate Path: ${templatePath}`;
+            throw new Error(err || 'Unable to download the template repository. Please contact the author if you see this error.');
         }
+
         SpinnerHelper.stop();
-        SpinnerHelper.init(`Installing dependency`);
+        SpinnerHelper.init("⚙️ Installing dependencies");
         // downloading dependencies
         let exitCode = await runCommand(`cd ${projectPath} && npm i`);
         SpinnerHelper.stop();
@@ -39,11 +41,11 @@ export const createProject = async function (type, folderName) {
             console.log(`Unable to install dependencies, process exited with code ${exitCode.toString()}`)
         }
         else {
-            console.log(`${EOL}new project '${projectPath}' initiated`);
+            console.log(`${EOL}🚀 New project '${projectPath}' initiated!`);
             if (folderName) {
-                console.log(`Execute command 'cd ${folderName}' to enter into project directory.`)
+                console.log(`📁 Execute 'cd ${folderName}' to enter into the project directory.`);
             }
-            console.log(`Execute command - 'fort-creator dev' for startin dev server.`);
+            console.log(`🛠 Execute 'fort-creator dev' to start the development server.`);
         }
     }
     catch (err) {
